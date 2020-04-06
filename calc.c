dd7fc45220422e62e82bd66509f4067277048ac5
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2020-04-06 16:31:37 +0900

    implement decimal calc

--- calc.c
+++ calc.c
@@ -3,18 +3,25 @@
 /*-------------------------------*/
 #define MAX_LENGTH 10
 
-int myatoi(char *s){
-    int value=0;
+float myatoi(char *s){
+    int dec_cnt=0;
+    float value=0;
     
     while(*s != '\n'){
-        value = value*10 + (*s - '0');
+        if(*s != '.'){
+            value = value*10 + (*s - '0');
+        } else {
+            dec_cnt = -1;
+        }
         s++;
+        dec_cnt++;
     }
+    value = value*dec_cnt*0.1;
     return value;
 }
 
-int calc(char *op, int arg1, int arg2){
-    int result=0;
+float calc(char *op, float arg1, float arg2){
+    float result=0;
     switch(op[0]){
         case '+':
             result = arg1 + arg2;
@@ -38,28 +45,29 @@ int calc(char *op, int arg1, int arg2){
 }
 
 int main(){
-    int i_arg1=0, i_arg2=0, i;
+    int i;
+    float i_arg1=0, i_arg2=0;
     char c_arg1[MAX_LENGTH], c_arg2[MAX_LENGTH], op[MAX_LENGTH];
 
 //set first argment
     read(0, c_arg1, MAX_LENGTH);
     i_arg1 = myatoi(c_arg1);
-    printf("%d\n", i_arg1);
+    printf("%f\n", i_arg1);
 
 while(1){
     //set operator
         read(0, op, 2);
         op[1] = '\0';
-        printf("%d %s\n", i_arg1, op);
+        printf("%f %s\n", i_arg1, op);
 
     //set second argment
         read(0, c_arg2, MAX_LENGTH);
         i_arg2 = myatoi(c_arg2);
-        printf("%d %s %d\n", i_arg1, op, i_arg2);
+        printf("%f %s %f\n", i_arg1, op, i_arg2);
 
     //result and set first argment
         i_arg1 = calc(op, i_arg1, i_arg2);    
-        printf("= %d\n", i_arg1);
+        printf("= %f\n", i_arg1);
     }  
 
         return 0;
