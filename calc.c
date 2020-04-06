99b089f6543b0075b1ec55943dcca6ae367f92da
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2020-04-06 15:30:19 +0900

    implement calc part

--- calc.c
+++ calc.c
@@ -8,10 +8,33 @@ int myatoi(char *s){
     
     while(*s != '\n'){
         value = value*10 + (*s - '0');
-        printf("%d\n",value);
         s++;
     }
     return value;
+}
+
+int calc(*c, arg1, arg2){
+    int result=0;
+    switch(c){
+        case '+':
+            result = arg1 + arg2;
+            break;
+        case '-':
+            result = arg1 - arg2;
+            break;
+        case '*':
+            result = arg1 * arg2;
+            break;
+        case '/':
+            result = arg1 / arg2;
+            break;
+        case 'r':
+            result = arg1;
+            break;
+        default:
+            printf("error\n");
+    }
+    return result;
 }
 
 int main(){
@@ -21,15 +44,22 @@ int main(){
 //set first argment
     read(0, c_arg1, MAX_LENGTH);
     i_arg1 = myatoi(c_arg1);
-    printf("%d", i_arg1);
-/*
-//set operator
-    read(0, c_op, MAX_LENGTH);
-    printf("%c ", c_op);  
+    printf("%d\n", i_arg1);
 
-//set second argment
-    read(0, c_arg2,MAX_LENGTH);
-    i_arg2 = myatoi(c_arg2);
-    printf("%d ", i_arg2);
-    */
+while(1){
+    //set operator
+        read(0, c_op, MAX_LENGTH);
+        printf("%d %c\n", i_arg1, c_op);
+
+    //set second argment
+        read(0, c_arg2, MAX_LENGTH);
+        i_arg2 = myatoi(c_arg2);
+        printf("%d %c %d\n", i_arg1, c_op, i_arg2);
+
+    //result and set first argment
+        i_arg1 = calc(c_op, arg1, arg2);    
+        printf("%d\n", i_arg1);
+    }  
+
+        return 0;
 }
