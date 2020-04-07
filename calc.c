82fc92191e16e30fbbc26c0adf9e6082444b5302
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2020-04-07 12:35:13 +0900

    implement strcmp, strcpy

--- calc.c
+++ calc.c
@@ -19,16 +19,16 @@ enum CMD {
 };
 
 //Char -> Float
-float myatoi(char *s){
+float myatof(char *s){
     int dec_cnt=0, i=0;
     float value=0, i_value=0, d_value=0;
     
-    while(*s != '\n'){
+    while(*s != '\0'){
         i_value = i_value*10 + (*s - '0');
         s++;
         if(*s == '.'){
             s++;
-            while(*s != '\n'){
+            while(*s != '\0'){
             //一番大きい桁から10倍して足していく
                 d_value = d_value*10 + (*s - '0');
                 s++;
@@ -44,6 +44,22 @@ float myatoi(char *s){
     }
     value = i_value + d_value;
     return value;
+}
+
+int mystrcmp(char *s1, char *s2){
+    while(*s1 == *s2){
+        if(*s1 == '\0')
+            return 0;
+        s1++;
+        s2++;
+    }
+    return 1;
+}
+
+void mystrcpy(char *s1, char *s2){
+    char *s = s1;
+    while((*s++ = *s2++) != 0);
+    return ;
 }
 
 //Calculate +,-,*,/,root
@@ -73,12 +89,19 @@ float calc(char *op, float arg1, float arg2){
 
 //入力管理
 int input_ctrl(char *s, FLAG flags){
+    int i=0;
     read(0, s, MAX_LENGTH);
+
+    while(s[i] != '\n'){
+        i++;
+    }
+    s[i] = '\0';
+
     if(!(flags.i_flag) && !(flags.op_flag) && !(flags.cmd_flag)){
         return init;
     } else if('0' <= s[0] && s[0] <= '9' && flags.i_flag){
         return integer;
-    } else if(s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/' || s[0] == 'r' && flags.op_flag){
+    } else if(!mystrcmp(s, "+") || !mystrcmp(s, "-") || !mystrcmp(s, "*") || !mystrcmp(s, "/") || !mystrcmp(s, "r") && flags.op_flag){
         return operator;
     } else if (s[0] == ':' && flags.cmd_flag){
         switch(s[1]){
@@ -103,7 +126,7 @@ int main(){
     while(1){
         switch(input_ctrl(arg, flags)){
             case init:
-                i_arg1 = myatoi(arg);
+                i_arg1 = myatof(arg);
                 printf("%f\n", i_arg1);
                 flags.i_flag = 0;
                 flags.op_flag = 1;
@@ -111,7 +134,7 @@ int main(){
                 break;
 
             case integer:
-                i_arg2 = myatoi(arg);
+                i_arg2 = myatof(arg);
                 printf("%f %s %f\n", i_arg1, op, i_arg2);
                 i_arg1 = calc(op, i_arg1, i_arg2);    
                 printf("= %f\n", i_arg1);
@@ -121,8 +144,7 @@ int main(){
                 break;
 
             case operator:
-                op[0] = arg[0];
-                op[1] = '\0';
+                mystrcpy(op, arg);
                 printf("%f %s\n", i_arg1, op);
                 flags.i_flag = 1;
                 flags.op_flag = 0;
